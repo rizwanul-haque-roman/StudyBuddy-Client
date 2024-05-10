845f33d8c843ef9187ff0a4a844ce577cf05378e
Rijwanul Haque Roman <rizwanul210058@diit.edu.bd>
2024-05-10 12:26:53 +0600

    banner section basic design and structure done

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -1,13 +1,36 @@
 import learner from "../../assets/banner_img.png";
+import blob from "../../assets/blob.svg";
 
 const Banner = () => {
   return (
-    <div className="bg-[#e0dede]">
-      <div className="h-screen  container mx-auto flex justify-between items-center">
+    <div className="bg-[#fff]">
+      {/* <svg
+        className="w-3/4 absolute -right-[400px] -top-[350px] -z-[0] rotate-45 overflow-x-hidden"
+        viewBox="0 0 200 200"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fill="#F2F4F8"
+          d="M30.6,-50.7C42.2,-46.3,55.8,-43.2,63.9,-34.9C72,-26.7,74.7,-13.3,76.1,0.8C77.4,15,77.6,29.9,69.4,38.1C61.3,46.4,44.9,47.9,32,55.2C19,62.6,9.5,75.7,-2.8,80.5C-15.1,85.4,-30.2,81.9,-39.4,72.4C-48.5,62.9,-51.7,47.3,-54.4,34.2C-57.2,21.1,-59.6,10.6,-63.1,-2C-66.7,-14.6,-71.3,-29.3,-68.5,-42.2C-65.7,-55.2,-55.3,-66.5,-42.6,-70.3C-30,-74,-15,-70.2,-2.7,-65.5C9.5,-60.8,19,-55.1,30.6,-50.7Z"
+          transform="translate(100 100)"
+        />
+      </svg> */}
+      <img
+        className="absolute -z-[0] top-0 right-0 overflow-x-hidden"
+        src={blob}
+        alt=""
+      />
+      <div className="h-screen container mx-auto flex justify-between items-center">
         <div>
-          <h1>Let&apos;s Grow Your Education Level up with E-Learning</h1>
+          <h1 className="text-7xl font-extrabold lg:leading-[100px]">
+            Let&apos;s Grow Your <br /> Education Level up <br /> with
+            E-Learning
+          </h1>
+          <button className="btn bg-[#F7BA33] hover:bg-[#F7BA33] text-white font-bold text-xl border-none mt-20 drop-shadow-[0_8px_8px_rgba(247,186,51)]">
+            Get started
+          </button>
         </div>
-        <img src={learner} alt="" />
+        <img className="z-10" src={learner} alt="" />
       </div>
     </div>
   );
--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
-    <div>
-      <div className="container mx-auto navbar">
+    <div className="absolute w-full z-50">
+      <div className=" container mx-auto navbar">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -69,7 +69,7 @@ const Navbar = () => {
           </ul>
         </div>
         <div className="navbar-end">
-          <a className="btn">Button</a>
+          <a className="btn">Sign In</a>
         </div>
       </div>
     </div>
